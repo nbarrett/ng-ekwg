bd0612ccf6d2e3384c6e147f67f10f0ee3abde04
nbarrett <nick.barrett@adaptassure.com>
2019-07-19 22:16:28 +0100

    - rollback to only url and zone polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,30 +18,25 @@
  * BROWSER POLYFILLS
  */
 
-/**
- * Web Animations `@angular/platform-browser/animations`
- * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
- * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
- */
 import "url-polyfill";
-import "core-js/es6/symbol";
-import "core-js/es6/object";
-import "core-js/es6/function";
-import "core-js/es6/parse-int";
-import "core-js/es6/parse-float";
-import "core-js/es6/number";
-import "core-js/es6/math";
-import "core-js/es6/string";
-import "core-js/es6/date";
-import "core-js/es6/array";
-import "core-js/es6/regexp";
-import "core-js/es6/map";
-import "core-js/es6/weak-map";
-import "core-js/es6/set";
-import "core-js/es6/array";
-import "classlist.js";  // Run `npm install --save classlist.js`.
-import "core-js/es6/reflect";
-import "web-animations-js";  // Run `npm install --save web-animations-js`.
+// import "core-js/es6/symbol";
+// import "core-js/es6/object";
+// import "core-js/es6/function";
+// import "core-js/es6/parse-int";
+// import "core-js/es6/parse-float";
+// import "core-js/es6/number";
+// import "core-js/es6/math";
+// import "core-js/es6/string";
+// import "core-js/es6/date";
+// import "core-js/es6/array";
+// import "core-js/es6/regexp";
+// import "core-js/es6/map";
+// import "core-js/es6/weak-map";
+// import "core-js/es6/set";
+// import "core-js/es6/array";
+// import "classlist.js";  // Run `npm install --save classlist.js`.
+// import "core-js/es6/reflect";
+// import "web-animations-js";  // Run `npm install --save web-animations-js`.
 import "zone.js/dist/zone";  // Included with Angular CLI.
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
