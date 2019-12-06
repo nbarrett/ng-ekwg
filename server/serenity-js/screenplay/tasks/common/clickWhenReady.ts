9576460123c023467176b5d8ffd7c38ea6bf5ee0
nbarrett <nick.barrett@adaptassure.com>
2019-12-06 20:58:10 +0000

    - fix ramblers export part 3 - wait for ramblers to finish processing - before clicking

--- server/serenity-js/screenplay/tasks/common/clickWhenReady.ts
+++ server/serenity-js/screenplay/tasks/common/clickWhenReady.ts
@@ -2,6 +2,7 @@ import { step } from "@serenity-js/core/lib/recording";
 import { PerformsTasks, Task, UsesAbilities } from "serenity-js/lib/screenplay";
 import { Click, Duration, Is, Target, Wait } from "serenity-js/lib/serenity-protractor";
 import { WalksTargets } from "../../ui/ramblers/walksTargets";
+import { WaitFor } from "../ramblers/common/waitFor";
 import { Hide } from "./hide";
 
 export class ClickWhenReady implements Task {
@@ -16,6 +17,7 @@ export class ClickWhenReady implements Task {
     @step("{0} clicks on #target when ready")
     performAs(actor: PerformsTasks & UsesAbilities): PromiseLike<void> {
         return actor.attemptsTo(
+            WaitFor.ramblersToFinishProcessing(),
             Hide.target(WalksTargets.chatWindow),
             Wait.upTo(Duration.ofSeconds(10)).until(this.target, Is.visible()),
             Click.on(this.target));
