ad144dd13a69b25423219facad1820d7e89cf6c1
nbarrett <nick.barrett36@me.com>
2020-02-29 14:36:18 +0000

    - migrator endpoint created for angular-js templates

--- server/lib/migration/template-migrator.js
+++ server/lib/migration/template-migrator.js
@@ -1,36 +1,92 @@
 "use strict";
-const config = require("../config/config.js");
+const {clone} = require("lodash");
+const config = require("../config/config");
+const stringUtils = require("../shared/string-utils");
 const debug = require("debug")(config.logNamespace("template-migrator"));
 let fs = require("fs");
+const path = require("path");
+const {resolve} = path;
+const {readdir, stat} = fs.promises;
 
 exports.migrateTemplate = (req, res) => {
-  const path = req.path;
-  const fileName = `../${req.query.file}`;
-  debug("migrateTemplate:file", fileName);
-  const response = {response: path, url: req.url, file: fileName};
-  if (fs.existsSync(fileName)) {
-    res.json(response)
-  } else {
-    response.error = fileName + " does not exist"
-    res.json(response)
+
+  async function walkDir(dir, filter) {
+    filter = filter || (() => true);
+    let list = await readdir(dir);
+    let results = await Promise.all(list.map(file => {
+      file = resolve(dir, file);
+      return stat(file).then(stat => {
+        if (stat.isDirectory()) {
+          return walkDir(file, filter);
+        } else {
+          return filter(file) ? file : "";
+        }
+      });
+    }));
+    return (await results.filter(f => !!f)).flat(2);
   }
 
-  // if (!fs.existsSync(path)) {
-  //   fs.mkdirSync(path);
-  // }
-  // fs.writeFile(filePath, csv, function (error) {
-  //   if (error) throw error;
-  //   debug("file", filePath, "saved");
-  //   process.env["RAMBLERS_USER"] = req.body.ramblersUser;
-  //   process.env["RAMBLERS_DELETE_WALKS"] = req.body.deleteWalks.join(",");
-  //   process.env["RAMBLERS_FILENAME"] = filePath;
-  //   process.env["RAMBLERS_WALKCOUNT"] = req.body.rows.length;
-  //   process.env["RAMBLERS_FEATURE"] = "walks-upload.ts";
-  //   const spawn = require("child_process").spawn;
-  //   debug("running feature", process.env["RAMBLERS_FEATURE"]);
-  //   const subprocess = spawn("npm", ["run", "e2e"], {
-  //     detached: true,
-  //     stdio: ["pipe"],
-  //   });
+  const tokenPairs = [
+    ["ng-if", "*ngIf"],
+    ["ng-disabled", "[disabled]"],
+    ["ng-click", "(click)"],
+    ["ng-hide", "!*ngIf"],
+    ["ng-model", "[(ngModel)]"],
+    ["ng-class", "[ngClass]"],
+    ["ng-bind", "[textContent]"],
+    ["ng-repeat", "*ngFor"],
+    ["ng-change", "(ngModelChange)"],
+    ["ng-href", "[href]"],
+    ["ng-options", "<option *ngFor"],
+    ["uib-", ""]];
+
+  const requestPath = req.path;
+  const fileName = `../${req.query.file}`;
+  const sourceDir = path.normalize(`../${req.query.in}`);
+  const migratedFileName = stringUtils.replaceAll("html", "ts", fileName);
+  debug("input file", fileName);
+  const response = {response: requestPath, in: sourceDir, out: req.query.out};
+  if (req.query.in) {
+    walkDir(sourceDir, (file) => file.endsWith(".html")).then(files => {
+      debug("files", files);
+      response.output = files.map(inputFile => {
+        const directory = path.dirname(inputFile);
+        const relativeDir = directory.split(req.query.in)[1];
+        const output = path.join("../", req.query.out, relativeDir, "/", path.basename(inputFile));
+        const outputFile = path.resolve(output);
+        convert(inputFile, outputFile);
+        return {
+          relativeDir,
+          inputFile,
+          inExists: fs.existsSync(inputFile),
+          outputFile,
+          outExists: fs.existsSync(output)
+        }
+      })
+      res.json(response)
+    })
+  } else {
+    res.json({error: "no input parameter specified"})
+  }
+
+  function convert(inputFile, outputFile) {
+    if (fs.existsSync(inputFile)) {
+      const outputDir = path.basename(outputFile);
+      if (!fs.existsSync(outputDir)) {
+        fs.mkdirSync(outputDir);
+      }
+      const fileContents = fs.readFileSync(inputFile).toString();
+      let outputContents = clone(fileContents);
+      tokenPairs.forEach(pair => {
+        debug("replacing tokens:", pair);
+        outputContents = stringUtils.replaceAll(pair[0], pair[1], outputContents);
+      })
+      debug("outputContents file", migratedFileName);
+      fs.writeFileSync(outputFile, outputContents);
+      debug("outputContents", migratedFileName, "created")
+    } else {
+      debug("input", inputFile, "does not exist")
+    }
+  }
 
 }
--- server/lib/shared/string-utils.js
+++ server/lib/shared/string-utils.js
@@ -1,7 +1,24 @@
+"use strict";
+const {escapeRegExp, isNumber} = require("lodash");
+
 exports.generateUid = () => {
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
     const r = Math.random() * 16 | 0;
     const v = c === "x" ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
+}
+
+exports.replaceAll = (find, replace, str) => {
+  let replacedValue;
+  let initialValue = "" + str;
+  while (true) {
+    replacedValue = initialValue.replace(new RegExp(escapeRegExp("" + find), "g"), replace);
+    if (replacedValue !== initialValue) {
+      initialValue = replacedValue;
+    } else {
+      break;
+    }
+  }
+  return isNumber(str) ? +replacedValue : replacedValue;
 }
