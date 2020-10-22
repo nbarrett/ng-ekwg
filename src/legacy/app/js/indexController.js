4124716cc667385455770ec1479e7409e8f7df06
nbarrett <nick.barrett36@me.com>
2020-10-22 01:23:21 +0100

    - removing legacy angular-js - remaining app.js files

--- src/legacy/app/js/indexController.js
+++
@@ -1,7 +0,0 @@
-angular.module('ekwgApp')
-  .controller("IndexController", function ($scope, PageService, DateUtils) {
-
-    $scope.year = DateUtils.asString(DateUtils.momentNow().valueOf(), undefined, "YYYY");
-
-  });
-
--- src/legacy/app/js/letterheadController.js
+++
@@ -1,6 +0,0 @@
-angular.module('ekwgApp')
-  .controller('LetterheadController', function ($scope, $location) {
-    var pathParts = $location.path().replace('/letterhead/', '').split('/');
-    $scope.firstPart = _.first(pathParts);
-    $scope.lastPart = _.last(pathParts);
-  });
