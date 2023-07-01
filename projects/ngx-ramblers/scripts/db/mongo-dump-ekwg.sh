7477b8e3f3a0def68a792d29eaf3533426c109e7
nbarrett <nick.barrett36@me.com>
2023-07-01 13:56:38 +0100

    - removed mongo dumps from git

--- projects/ngx-ramblers/scripts/db/mongo-dump-ekwg.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# Dump whole database
-mongodump -h ds063168.mongolab.com:63168 -d ekwg -u ekwg -p ekwg -o data/dump
--- projects/ngx-ramblers/scripts/db/mongo-dump-nbarrett.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# Dump whole database
-mongodump -h ds061258.mongolab.com:61258 -d nbarrett -u ekwg -p ekwg -o data/dump
